d0a1ab56e9e3f35bf57e49d0733093eae0e9599e
fabricio3254 <124294621+fabricio3254@users.noreply.github.com>
2023-06-01 15:49:28 -0300

    Add files via upload

---
+++ ReadTextFileTest.java
@@ -0,0 +1,7 @@
+public class ReadTextFileTest {
+    public static void main(String[] args)
+    {
+    ReadTextFile application = new ReadTextFile();
+    application.openFile(); application.readRecords(); application.closeFile();
+    } // fim de main
+    // fim da classe ReadTextFileTest
